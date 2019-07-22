fe13784e92f1fef7fea4f93bbc59982abbf4732a
Rahul <rahulsainani@wtinfotech.co.uk>
2019-07-22 22:39:07 +0200

    add base view model

---
+++ base/src/main/java/shapes/base/presentation/BaseViewModel.kt
@@ -0,0 +1,19 @@
+package shapes.base.presentation
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    protected fun Disposable.addToCompositeDisposable() {
+        compositeDisposable.add(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
