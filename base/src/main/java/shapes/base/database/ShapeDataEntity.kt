8e5cb3c5a076310bacbab2c56392d9bae89af89b
Rahul <rahulsainani@wtinfotech.co.uk>
2019-07-23 14:15:19 +0200

    update data entity

--- base/src/main/java/shapes/base/database/ShapeDataEntity.kt
+++ base/src/main/java/shapes/base/database/ShapeDataEntity.kt
@@ -5,10 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "shapes")
 data class ShapeDataEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val type: ShapeType,
-    val centerX: Float,
-    val centerY: Float
+    @PrimaryKey val id: Int,
+    val type: ShapeType
 ) {
     enum class ShapeType(val code: Int) {
         CIRCLE(VALUE_CIRCLE),
