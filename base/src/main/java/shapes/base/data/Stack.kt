0b0f4dcef1e8c321f652db2cd40c24772588b56a
Rahul <rahulsainani@wtinfotech.co.uk>
2019-07-23 21:23:11 +0200

    add stack to hold state for undo functionality

---
+++ base/src/main/java/shapes/base/data/Stack.kt
@@ -0,0 +1,24 @@
+package shapes.base.data
+
+import shapes.base.database.ShapeDataEntity
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Stack @Inject constructor() {
+
+    private val stack = ArrayDeque<List<ShapeDataEntity>>()
+
+    fun push(list: List<ShapeDataEntity>) {
+        stack.push(list)
+    }
+
+    fun pop(): List<ShapeDataEntity>? {
+        if (stack.size > 0) {
+            return stack.pop()
+        }
+        return null
+    }
+
+}
--- base/src/main/java/shapes/base/di/component/ApplicationComponent.kt
+++ base/src/main/java/shapes/base/di/component/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package shapes.base.di.component
 
 import dagger.Component
+import shapes.base.data.Stack
 import shapes.base.database.ShapesDao
 import shapes.base.di.module.ApplicationModule
 import javax.inject.Singleton
@@ -9,4 +10,5 @@ import javax.inject.Singleton
 @Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
     fun shapesDao(): ShapesDao
+    fun stack(): Stack
 }
