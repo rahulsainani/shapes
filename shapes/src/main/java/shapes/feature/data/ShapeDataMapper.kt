d957cba60c6ca57a94117e983b74284bcef538d0
Rahul <rahulsainani5@gmail.com>
2019-07-25 23:42:29 +0200

    update shapes data mapper to use type mapper

--- shapes/src/main/java/shapes/feature/data/ShapeDataMapper.kt
+++ shapes/src/main/java/shapes/feature/data/ShapeDataMapper.kt
@@ -2,22 +2,16 @@ package shapes.feature.data
 
 import io.reactivex.functions.Function
 import shapes.base.database.ShapeDataEntity
-import shapes.base.database.ShapeDataEntity.ShapeType
 import shapes.feature.domain.ShapeDomainEntity
 import javax.inject.Inject
 
-class ShapeDataMapper @Inject constructor() : Function<ShapeDomainEntity, ShapeDataEntity> {
+class ShapeDataMapper @Inject constructor(
+    private val shapeTypeDataMapper: ShapeTypeDataMapper
+) : Function<ShapeDomainEntity, ShapeDataEntity> {
 
-    override fun apply(domainEntity: ShapeDomainEntity): ShapeDataEntity {
-        val type = when (domainEntity.type) {
-            ShapeDomainEntity.Type.CIRCLE -> ShapeType.CIRCLE
-            ShapeDomainEntity.Type.SQUARE -> ShapeType.SQUARE
-            ShapeDomainEntity.Type.TRIANGLE -> ShapeType.TRIANGLE
-        }
-
-        return ShapeDataEntity(
+    override fun apply(domainEntity: ShapeDomainEntity): ShapeDataEntity =
+        ShapeDataEntity(
             id = domainEntity.id,
-            type = type
+            type = shapeTypeDataMapper.apply(domainEntity.type)
         )
-    }
 }
--- shapes/src/test/java/shapes/feature/data/ShapeDataMapperTest.kt
+++ shapes/src/test/java/shapes/feature/data/ShapeDataMapperTest.kt
@@ -1,17 +1,22 @@
 package shapes.feature.data
 
-import org.junit.jupiter.api.Assertions.*
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import shapes.feature.TestObject
 
 internal class ShapeDataMapperTest {
 
-    private val tested = ShapeDataMapper()
+    private val shapeTypeDataMapper: ShapeTypeDataMapper = mock()
+    private val tested = ShapeDataMapper(shapeTypeDataMapper)
 
     @Test
     fun `should map shape domain entity to data entity`() {
         val domainEntity = TestObject.shapeDomainEntity()
         val expected = TestObject.shapeDataEntity()
+
+        whenever(shapeTypeDataMapper.apply(domainEntity.type)).thenReturn(expected.type)
 
         assertEquals(expected, tested.apply(domainEntity))
     }
