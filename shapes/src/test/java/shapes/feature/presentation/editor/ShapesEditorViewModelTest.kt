f796ed2350aed87dcac3fa718f374cb05ca47021
Rahul <rahulsainani5@gmail.com>
2019-07-29 22:46:38 +0200

    add tests for view model

---
+++ shapes/src/test/java/shapes/feature/presentation/editor/ShapesEditorViewModelTest.kt
@@ -0,0 +1,110 @@
+package shapes.feature.presentation.editor
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.schedulers.TestScheduler
+import io.reactivex.subjects.BehaviorSubject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import shapes.base.rx.SchedulerProvider
+import shapes.feature.TestObject
+import shapes.feature.core.InstantTask
+import shapes.feature.domain.*
+import java.util.concurrent.TimeUnit
+
+@InstantTask
+internal class ShapesEditorViewModelTest {
+
+    private val schedulerProvider: SchedulerProvider = mock()
+    private val retrieveShapes: RetrieveShapes = mock()
+    private val addShape: AddShape = mock()
+    private val switchShape: SwitchShape = mock()
+    private val deleteShape: DeleteShape = mock()
+    private val undoLastAction: UndoLastAction = mock()
+
+    private lateinit var tested: ShapesEditorViewModel
+
+    private val shapesList = mock<List<ShapeDomainEntity>>()
+    private val shapesSteam = BehaviorSubject.create<List<ShapeDomainEntity>>()
+    private val testScheduler = TestScheduler()
+
+    @BeforeEach
+    fun setup() {
+        whenever(retrieveShapes.retrieveShapes())
+            .thenReturn(shapesSteam.toFlowable(BackpressureStrategy.LATEST))
+        whenever(schedulerProvider.computation()).thenReturn(testScheduler)
+        tested = ShapesEditorViewModel(
+            retrieveShapes, addShape, switchShape, deleteShape, undoLastAction, schedulerProvider
+        )
+    }
+
+    @Test
+    fun `shapes list are posted to live data`() {
+        shapesSteam.onNext(shapesList)
+
+        testScheduler.advanceTimeBy(50, TimeUnit.MILLISECONDS)
+
+        assertEquals(shapesList, tested.shapesLiveData.value)
+    }
+
+    @Test
+    fun `add shape is called on triangle click`() {
+        whenever(addShape.addShape(any())).thenReturn(Completable.complete())
+
+        tested.onTriangleClick()
+
+        verify(addShape).addShape(ShapeDomainEntity.Type.TRIANGLE)
+    }
+
+    @Test
+    fun `add shape is called on circle click`() {
+        whenever(addShape.addShape(any())).thenReturn(Completable.complete())
+
+        tested.onCircleClick()
+
+        verify(addShape).addShape(ShapeDomainEntity.Type.CIRCLE)
+    }
+
+    @Test
+    fun `add shape is called on square click`() {
+        whenever(addShape.addShape(any())).thenReturn(Completable.complete())
+
+        tested.onSquareClick()
+
+        verify(addShape).addShape(ShapeDomainEntity.Type.SQUARE)
+    }
+
+    @Test
+    fun `switch shape is called on shape click`() {
+        val shapeDomainEntity = TestObject.shapeDomainEntity()
+        whenever(switchShape.switchShape(any())).thenReturn(Completable.complete())
+
+        tested.onShapeClick(shapeDomainEntity)
+
+        verify(switchShape).switchShape(shapeDomainEntity)
+    }
+
+    @Test
+    fun `delete shape is called on shape long click`() {
+        val shapeDomainEntity = TestObject.shapeDomainEntity()
+        whenever(deleteShape.delete(any())).thenReturn(Completable.complete())
+
+        tested.onShapeLongClick(shapeDomainEntity)
+
+        verify(deleteShape).delete(shapeDomainEntity)
+    }
+
+    @Test
+    fun `undo is called on undo click`() {
+        whenever(undoLastAction.undo()).thenReturn(Completable.complete())
+
+        tested.onUndoClick()
+
+        verify(undoLastAction).undo()
+    }
+}
---
+++ shapes/src/test/java/shapes/feature/presentation/stats/StatisticsViewModelTest.kt
@@ -0,0 +1,60 @@
+package shapes.feature.presentation.stats
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.subjects.BehaviorSubject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import shapes.feature.core.InstantTask
+import shapes.feature.domain.DeleteAllShapesByType
+import shapes.feature.domain.RetrieveShapes
+import shapes.feature.domain.ShapeDomainEntity
+
+@InstantTask
+internal class StatisticsViewModelTest {
+
+    private val retrieveShapes: RetrieveShapes = mock()
+    private val deleteAllShapesByType: DeleteAllShapesByType = mock()
+    private val statisticsViewEntityMapper: StatisticsViewEntityMapper = mock()
+
+    private val shapesSteam = BehaviorSubject.create<List<ShapeDomainEntity>>()
+
+    private lateinit var tested: StatisticsViewModel
+
+    private val shapesList = mock<List<ShapeDomainEntity>>()
+    private val statItemEntities = mock<List<StatisticsItemEntity>>()
+
+    @BeforeEach
+    fun setup() {
+        whenever(retrieveShapes.retrieveShapes())
+            .thenReturn(shapesSteam.toFlowable(BackpressureStrategy.LATEST))
+
+        tested = StatisticsViewModel(
+            retrieveShapes, deleteAllShapesByType, statisticsViewEntityMapper
+        )
+    }
+
+    @Test
+    fun `number of shapes is posted on live data`() {
+        whenever(statisticsViewEntityMapper.apply(shapesList)).thenReturn(statItemEntities)
+
+        shapesSteam.onNext(shapesList)
+
+        Assertions.assertEquals(statItemEntities, tested.statsListLiveData.value)
+    }
+
+    @Test
+    fun `delete all shapes of type is called on item click`() {
+        val shapeType = ShapeDomainEntity.Type.TRIANGLE
+        whenever(deleteAllShapesByType.delete(any())).thenReturn(Completable.complete())
+
+        tested.onItemClick(shapeType)
+
+        verify(deleteAllShapesByType).delete(shapeType)
+    }
+}
